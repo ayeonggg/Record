5203b6fa4031e6218e6a080af14fb4365829bf90
ayeonggg <101121988+ayeonggg@users.noreply.github.com>
2025-08-06 03:06:21 -0700

    Update video_plugin.py

--- my_yolo_rqt_plugin/catkin_ws/src/rqt_image_view_ready/src/my_rqt_video_plugin/video_plugin.py
+++ my_yolo_rqt_plugin/catkin_ws/src/rqt_image_view_ready/src/my_rqt_video_plugin/video_plugin.py
@@ -1,66 +1,71 @@
 #!/usr/bin/env python3
 import os
 import rospy
-import cv2
 from python_qt_binding.QtWidgets import QWidget, QLabel, QPushButton
-from python_qt_binding.QtCore import QTimer, Qt
 from python_qt_binding import loadUi
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import Qt
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from rqt_gui_py.plugin import Plugin
 
 class VideoPlugin(Plugin):
-
     def __init__(self, context):
         super(VideoPlugin, self).__init__(context)
         self.setObjectName('MyPlugin')
-        
-        self._widget = QWidget()
-        ui_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), '/home/kitech/catkin_ws/src/rqt_image_view_ready/resource/streaming.ui')
-        loadUi(ui_file, self._widget)
-        if context.serial_number() > 1:
-            self._widget.setWindowTitle(f'{self._widget.windowTitle()} ({context.serial_number()})')
 
+        # Load UI
+        self._widget = QWidget()
+        ui_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                               '/home/ayeong/my_yolo_rqt_plugin/catkin_ws/src/rqt_image_view_ready/resource/streaming.ui')
+        loadUi(ui_file, self._widget)
         context.add_widget(self._widget)
 
         self.bridge = CvBridge()
-        self.play_button = self._widget.findChild(QPushButton, 'play_button')
-        self.video_label = self._widget.findChild(QLabel, 'video_label')
+        self.sub_rgb = None
+        self.sub_meta = None
 
-        self.play_button.clicked.connect(self.start_video)
+        # Find widgets
+        self.button_rgb = self._widget.findChild(QPushButton, 'pushButton')
+        self.button_meta = self._widget.findChild(QPushButton, 'pushButton_2')
+        self.label_rgb = QLabel()
+        self.label_meta = QLabel()
 
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.update_frame)
-        self.cap = None
-        self.subscriber = rospy.Subscriber('/video_frames', Image, self.image_callback)
-        
-    def image_callback(self, data):
+        layout_rgb = self._widget.findChild(QWidget, 'verticalLayoutWidget').layout()
+        layout_meta = self._widget.findChild(QWidget, 'verticalLayoutWidget_2').layout()
+
+        self.label_rgb.setFixedSize(320, 240)
+        self.label_meta.setFixedSize(320, 240)
+        self.label_rgb.setStyleSheet("background-color: black")
+        self.label_meta.setStyleSheet("background-color: black")
+
+        layout_rgb.addWidget(self.label_rgb)
+        layout_meta.addWidget(self.label_meta)
+
+        self.button_rgb.clicked.connect(self.subscribe_rgb)
+        self.button_meta.clicked.connect(self.subscribe_meta)
+
+    def subscribe_rgb(self):
+        if not self.sub_rgb:
+            self.sub_rgb = rospy.Subscriber("/video_rgb", Image, self.cb_rgb)
+
+    def subscribe_meta(self):
+        if not self.sub_meta:
+            self.sub_meta = rospy.Subscriber("/video_meta", Image, self.cb_meta)
+
+    def cb_rgb(self, msg):
+        self.update_image(msg, self.label_rgb)
+
+    def cb_meta(self, msg):
+        self.update_image(msg, self.label_meta)
+
+    def update_image(self, msg, label):
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-            rgb_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
-            h, w, ch = rgb_image.shape
-
-            qt_img = QImage(rgb_image.data, w, h, ch * w, QImage.Format_RGB888)
-            self.video_label.setPixmap(QPixmap.fromImage(qt_img))
+            img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            h, w, ch = img.shape
+            qimg = QImage(img.data, w, h, ch * w, QImage.Format_RGB888)
+            label.setPixmap(QPixmap.fromImage(qimg).scaled(
+                label.width(), label.height(), Qt.KeepAspectRatio))
         except Exception as e:
-            rospy.logerr(f"Image conversion error: {e}")
-    def start_video(self):
-        video_path = "/home/kitech/catkin_ws/src/rqt_image_view_ready/SHENZHEN.mp4"
-        if not os.path.exists(video_path):
-            rospy.logerr(f"Video file not found: {video_path}")
-            return
-        self.cap = cv2.VideoCapture(video_path)
-        self.timer.start(30)  # 대략 30FPS
-
-    def update_frame(self):
-        if self.cap is not None and self.cap.isOpened():
-            ret, frame = self.cap.read()
-            if ret:
-                rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                h, w, ch = rgb_image.shape
-                from PyQt5.QtGui import QImage, QPixmap
-                qt_img = QImage(rgb_image.data, w, h, ch * w, QImage.Format_RGB888)
-                self.video_label.setPixmap(QPixmap.fromImage(qt_img))
-            else:
-                self.timer.stop()
-                self.cap.release()
+            rospy.logerr(f"Image update error: {e}")
